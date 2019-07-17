0296485dea8282fd3c535c1dfe57cde0a311aa5c
chnhuangbo <chnhuangbo@qq.com>
2019-07-18 02:47:13 +0800

    first commit

---
+++ ripple-blog-web/src/main/java/com/huangbo/rippleblog/RippleblogApplication.java
@@ -0,0 +1,13 @@
+package com.huangbo.rippleblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RippleblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RippleblogApplication.class, args);
+    }
+
+}
